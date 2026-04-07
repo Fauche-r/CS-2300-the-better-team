4c4d74e16cfc866ea187d0ec25b42d6024ac4526
Fauche-r <fauxcheur597@gmail.com>
2026-04-07 11:13:05 -0400

    Fixed even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -4,17 +4,17 @@ import java.io.*;
 
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
-        BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
+        BufferedWriter file = new BufferedWriter(new FileWriter("numbers"));
         //Write the Title
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
         //For each number, if it is even write it on the left column and if it is odd write on the right column
         for (int i = 1; i <= 50; i++) {
-            if (i % 2 == 1) {
+            if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
             }
             else {
-                file.rite("<tr><td></td><td>" + i + "</td></tr>\n");
+                file.write("<tr><td></td><td>" + i + "</td></tr>\n");
             }
         }
         file.write("</table>\n</body>\n</html>");
@@ -22,7 +22,7 @@ public class even_odd_site {
         BufferedReader reader = new BufferedReader(new FileReader("numbers"));
         String line;
         //Print out each line in the table
-        while ((line = reader.readLine()) == null) {
+        while ((line = reader.readLine()) != null) {
             System.out.println(line);
         }
         reader.close();
